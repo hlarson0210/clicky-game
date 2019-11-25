828137d028102728518cfc3c476485edbe2b06ab
Hayden Larson <hlarson398@gmail.com>
2019-11-24 21:19:03 -0600

    added the build folder

---
+++ build/precache-manifest.20a37b333a79b59422a114159726f2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b003432cecc35ae334254f1f16c9876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b7efe41c7dc49cad38",
+    "url": "/static/css/main.96127157.chunk.css"
+  },
+  {
+    "revision": "fd375e3b08406b5b2a06",
+    "url": "/static/js/2.34e1a87d.chunk.js"
+  },
+  {
+    "revision": "59b7efe41c7dc49cad38",
+    "url": "/static/js/main.443ad859.chunk.js"
+  },
+  {
+    "revision": "93178cac179f84ce7888",
+    "url": "/static/js/runtime-main.15eb2435.js"
+  }
+]);
