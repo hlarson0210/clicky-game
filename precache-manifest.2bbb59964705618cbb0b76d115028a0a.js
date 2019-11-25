743114e076b130817bf0f1637141ab8378a78cce
Hayden Larson <hlarson398@gmail.com>
2019-11-24 21:33:20 -0600

    Updates

---
+++ precache-manifest.2bbb59964705618cbb0b76d115028a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3acb842661211ebd360ae01b8a60da7f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "59b7efe41c7dc49cad38",
+    "url": "/clicky-game/static/css/main.96127157.chunk.css"
+  },
+  {
+    "revision": "fd375e3b08406b5b2a06",
+    "url": "/clicky-game/static/js/2.34e1a87d.chunk.js"
+  },
+  {
+    "revision": "59b7efe41c7dc49cad38",
+    "url": "/clicky-game/static/js/main.443ad859.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  }
+]);
